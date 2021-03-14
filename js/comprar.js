e9ac9f349279df76da9139d52da040a8ac64d549
Antonella <antonella98.herrera@gmail.com>
2021-03-13 22:50:12 -0300

    ultimos arreglos

--- js/comprar.js
+++ js/comprar.js
@@ -5,20 +5,26 @@ function quieroComprar() {
     if (carrito.length <= 3) {
         let productoElegido = prompt(`Si quieres comprar un jean escriba: Jean, si quiere una remera escriba: Remera`)
         if (productoElegido == "Jean") {
-            carrito.push(pantalonInquita.nombre);
-            descuentoDeJean("Jean")
-            alert(`Este producto tiene un %20 de descuento, entonces vale ${pantalonInquitaConDescuento}`)
-            console.log(`${pantalonInquita.nombre} fue agregado al carrito ($${pantalonInquitaConDescuento})`)
-        } 
-        else if (productoElegido == "Remera") {
-            carrito.push(remerasCartoon.nombre);
-            console.log(`${remerasCartoon.nombre} fue agregado al carrito ($${remerasCartoon.precio})`);
-            alert(`Se eligio Remera, el cual tiene un precio de $${remerasCartoon.precio}`)
+            carrito.push(pantalonInquita.nombre)
+            descuentoDeJean()
+            alert(`Estes producto se agrego con un 20% de descuento, entonces vale ${pantalonInquitaConDescuento}`)
+            console.log(`${pantalonInquita.nombre} fue agregado al carrito`)
             seguirComprando()
+        }
+        else if (productoElegido == "Remera") {
+        carrito.push(remerasCartoon.nombre);
+        console.log(`${remerasCartoon.nombre} fue agregado al carrito ($${remerasCartoon.precio})`);
+        alert(`Se eligio Remera, el cual tiene un precio de $${remerasCartoon.precio}`)
+        seguirComprando()
         } else {
-            alert(`El producto elegido no fue encontrado`)
+        alert(`El producto elegido no fue encontrado`)
         }
     } else {
         carritoLleno();
     }
 }
+
+
+
+
+
--- js/funcionesCompra.js
+++ js/funcionesCompra.js
@@ -1,21 +1,16 @@
 function carritoLleno() {
-    alert(`¡SU CARRITO ESTA LLENO! Estos son sus productos: ${carrito.toString()} y el total de su compra es {sumaTotal}`);
+    alert(`¡SU CARRITO ESTA LLENO! Estos son sus productos: ${carrito.toString()}`);
     console.warn("El carrito esta lleno");
-    console.info(`en el carrito se encuentra ${carrito[0]}, y el total de la compra es ${sumaTotal}`);
+    console.info(`en el carrito se encuentra ${carrito[0]}`);
 }
 
 function seguirComprando() {
-    if( carrito.length<=3){
-        let siQuieroComprar= window.confirm('¿Quieres seguir comprando?');
-        if(siQuieroComprar=true){
+    if( carrito.length <=3){
+        let siQuierocomprar = window.confirm(`¿Queres seguir comprando?`);
+        if (siQuierocomprar == true) {
             quieroComprar()
-        }else{
-            if(carrito.length=1){
-                alert(`¡Gracias por tu compra! Tiene un producto en el carrito: ${carrito[0]}`);
-               console.info(`en el carrito se encuentra ${carrito[0]}`);
-            }else{
-                alert(`¡Gracias por tu compra! Estos son sus productos: ${carrito.toString()}`);
-            }
+        } else {
+            alert(`¡Gracias por tu compra, en el carrto actualmente se encuentran los productos: ${carrito.toString()}!`)
         }
     }else{
         carritoLleno();
